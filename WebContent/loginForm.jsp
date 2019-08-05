0210d1459feb1641169e6ea688a0d3d9bd0be40e
kimseungki94 <skkim0832@gmail.com>
2019-08-06 01:20:21 +0900

    first commit

---
+++ WebContent/loginForm.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+	table{
+		background-color: pink;
+	}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>로그인</h2>
+<form action="loginPro.jsp">
+<table border="1">
+	<tr><td>아이디</td><td><input type="text" name="id" required="required"></td></tr>
+	<tr><td>암호</td><td><input type="password" name="passwd" required="required"></td></tr>
+	<tr><td></td><td><input type="submit" value="확인"></td>
+	<td><input type="submit" value="확인"></td></tr>
+</table>
+</form><p>
+
+<input type="button" value="회원가입" onclick="location.href='joinForm.jsp'">
+</body>
+</html>
---
+++ src/och12/MemberDao.java
@@ -0,0 +1,102 @@
+package och12;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDao {
+	private static MemberDao instance;
+	private MemberDao() {}
+	public static MemberDao getInstance() {
+		if(instance == null) {
+			instance = new MemberDao();
+		}
+		return instance;
+		
+	}
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/OracleDB");
+			conn = ds.getConnection();
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+	public int check(String id,String passwd) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int result = 0;
+		String sql1 = "select passwd from member2 where id=?";
+		try {
+			String dbPasswd = "";
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql1);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				dbPasswd = rs.getString(1);
+				System.out.println("디비에 저장된 비밀번호 : " +dbPasswd);
+				System.out.println("당신이 입력한 비밀번호 : " +passwd);
+				if (dbPasswd.equals(passwd)) {			
+					pstmt.setString(1, id);
+					result = 1;
+				} else result = 0;
+			} else result = -1;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+			if (rs != null)
+				rs.close();
+		}
+		return result;
+	}
+	public List<Member> list() throws SQLException {
+		List<Member> list = new ArrayList<Member>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null; 
+		String sql = "select * from member2";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Member board = new Member();
+				board.setId(rs.getString("id"));
+				//board.setId(rs.getString(1));
+				board.setPasswd(rs.getString("passwd"));
+				board.setName(rs.getString("name"));
+				board.setAddress(rs.getString("address"));
+				board.setTel(rs.getString("tel"));
+				board.setReg_date(rs.getDate("reg_date")); //
+				list.add(board); 
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return list;
+	}
+
+}
